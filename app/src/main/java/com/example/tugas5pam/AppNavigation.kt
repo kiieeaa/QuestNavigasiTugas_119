01c77c3f69dc7c19b224b28ad645c8c6570ef6e5
kiieeaa <kiyaadinda5@gmail.com>
2025-11-07 20:27:10 +0700

    menambahkan sealed class Screen untuk mendefinisikan rute tiap layar

---
+++ app/src/main/java/com/example/tugas5pam/AppNavigation.kt
@@ -0,0 +1,21 @@
+package com.example.tugas5pam
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+
+
+sealed class Screen(val route: String) {
+    object Splash : Screen("splash_screen")
+    object Registration : Screen("registration_screen")
+
+
+    object Details : Screen("details_screen/{nama}/{kelamin}/{status}/{alamat}") {
+        fun createRoute(nama: String, kelamin: String, status: String, alamat: String): String {
+            return "details_screen/$nama/$kelamin/$status/$alamat"
+        }
+    }
+}

1fa1f117289a96562a92c20c264a4d01ecaf5ddd
kiieeaa <kiyaadinda5@gmail.com>
2025-11-07 20:29:34 +0700

    mengintegrasikan Splash, Registration, dan Details screen ke NavHost

--- app/src/main/java/com/example/tugas5pam/AppNavigation.kt
+++ app/src/main/java/com/example/tugas5pam/AppNavigation.kt
@@ -22,3 +22,26 @@ sealed class Screen(val route: String) {
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Splash.route) {
+
+
+        composable(Screen.Splash.route) {
+            SplashScreen(navController = navController)
+        }
+
+
+        composable(Screen.Registration.route) {
+            RegistrationScreen(navController = navController)
+        }
+
+
+        composable(
+            route = Screen.Details.route,
+            arguments = listOf(
+                navArgument("nama") { type = NavType.StringType },
+                navArgument("kelamin") { type = NavType.StringType },
+                navArgument("status") { type = NavType.StringType },
+                navArgument("alamat") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+

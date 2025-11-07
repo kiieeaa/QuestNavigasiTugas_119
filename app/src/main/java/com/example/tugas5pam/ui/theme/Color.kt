d3a58e698bdf9f5594e645c6a3674201a4c88ecb
kiieeaa <kiyaadinda5@gmail.com>
2025-11-07 20:46:03 +0700

    menambahkan palet warna tema aplikasi untuk konsistensi desain UI

---
+++ app/src/main/java/com/example/tugas5pam/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+
+
+package com.example.tugas5pam.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val TealDark = Color(0xFF00695C)
+val TealMedium = Color(0xFF00897B)
+val TealButtonLight = Color(0xFF4DB6AC)
+val TealFieldBg = Color(0xFFB2DFDB)
+val TealAppBg = Color(0xFFE0F2F1)
+val TextGrey = Color(0xFF4A4A4A)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)

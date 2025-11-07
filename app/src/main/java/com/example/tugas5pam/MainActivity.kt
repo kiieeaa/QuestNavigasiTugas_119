5f3e7b8ce41495d14078388d8ea3be321d198891
kiieeaa <kiyaadinda5@gmail.com>
2025-11-07 20:06:16 +0700

    inisialisasi project dan setup struktur awal MainActivity

---
+++ app/src/main/java/com/example/tugas5pam/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tugas5pam
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.tugas5pam.ui.theme.TealAppBg
+import com.example.tugas5pam.ui.theme.Tugas5PAMTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
